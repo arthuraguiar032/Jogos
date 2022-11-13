1695e0a11da8df98ae36fd9914827a9122d39ac5
Arthur Aguiar <arthuraguiar@dcc.ufrj.br>
2022-11-13 15:58:55 -0300

    Update tresTrect.c

--- 01/tresRect.c
+++ 01/tresRect.c
@@ -1,8 +1,22 @@
-//#include <assert.h>
 #include <SDL2/SDL.h>
 
 const int SCREEN_WIDTH = 960;
 const int SCREEN_HEIGHT = 540;
+
+int aux_WaitEventTimeoutCount(SDL_Event* evt, Uint32* ms){
+	int antes = SDL_GetTicks();
+	int is_evt = SDL_WaitEventTimeout(evt, *ms);
+	int d_tempo = SDL_GetTicks() - antes;
+	
+	if (is_evt){
+		//protecao para caso haja algum ruido na contagem de tempo
+		if(d_tempo>*ms){
+			d_tempo = *ms;
+		}
+		*ms -= d_tempo;
+	}	
+	return is_evt;
+}
 
 int main (int argc, char* args[])
 {
@@ -14,39 +28,33 @@ int main (int argc, char* args[])
                          SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN
                       );
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
-
-	int quit = 0;
-    SDL_Event e;
 	
     /* EXECUÇÃO */
     SDL_Rect r_teclado = {40, 20, 30, 30};
     SDL_Rect r_mouse = {80, 40, 30, 30};
     SDL_Rect r_tempo = {120, 60, 30, 30};
     
-    int espera = 500;
+    int quit = 0;
+    SDL_Event e;
+    int espera_inicial = 5000;
+    Uint32 espera = espera_inicial;
+    
     while (!quit) {
+    	//desenhando os retangulos
         SDL_SetRenderDrawColor(ren, 0x00,0x00,0x00,0x00);
         SDL_RenderClear(ren);
-        //ffe4c4
         SDL_SetRenderDrawColor(ren, 0xFF,0xE4,0xC4,0x00);
         SDL_RenderFillRect(ren, &r_teclado);
-        //87cefa
         SDL_SetRenderDrawColor(ren, 0x84,0xCE,0xFA,0x00);
         SDL_RenderFillRect(ren, &r_mouse);
-        //c8a2c8
         SDL_SetRenderDrawColor(ren, 0xC8,0xA2,0xC8,0x00);
         SDL_RenderFillRect(ren, &r_tempo);
-        
         SDL_RenderPresent(ren);
 
         SDL_Event evt;
-        //Uint32 antes = SDL_GetTicks();
-        int isevt = SDL_WaitEventTimeout(&evt, espera);
+        int isevt = aux_WaitEventTimeoutCount(&evt, &espera);
+        
         if (isevt) {
-            /*espera -= (SDL_GetTicks() - antes);
-            if (espera < 0) {
-                espera = 0;
-            }*/
             if (evt.type == SDL_QUIT) {
             	quit = 1;
             }else if (evt.type == SDL_KEYDOWN) {
@@ -71,7 +79,8 @@ int main (int argc, char* args[])
             	r_mouse.y = y;
             }
         } else {
-            //espera = 500;
+        	//refefine o tempo de espera
+            espera = espera_inicial;
             r_tempo.x += 2;
             r_tempo.y += 2;
         }
